e1cea88774c72f188b2328831bc0b99a856e3af1
sunidhisingh66541 <sunidhikr2666@gmail.com>
2024-01-14 01:35:28 +0530

    this is rect file

---
+++ React/React/creative/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//import Scss
+import './assets/scss/themes.scss';
+
+//imoprt Route
+import Route from './Routes';
+
+// Fake Backend 
+import fakeBackend from "./helpers/AuthType/fakeBackend";
+
+// Activating fake backend
+fakeBackend();
+
+function App() {
+  return (
+    <React.Fragment>
+      <Route />
+    </React.Fragment>
+  );
+}
+
+export default App;
